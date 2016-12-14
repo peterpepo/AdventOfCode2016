4f181a237624cfe51ce2864ecc01b36a076fd611
Peter Pepo <peterpepo1@gmail.com>
2016-12-14 23:21:08 +0100

    Day-14: Cleanup, once again
    * Removes getNextLevel2016Hash from Md5HashGenerator - a leftover from
    ** initial state of solution, prior to refactoring

--- src/day14/Md5HashGenerator.java
+++ src/day14/Md5HashGenerator.java
@@ -26,27 +26,4 @@ public class Md5HashGenerator implements HashGenerator {
 
         return byteData;
     }
-
-    public byte[] getNextLevel2016Hash() {
-        byte[] byteData = this.getNextHash();
-
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("MD5");
-
-//            byte[] byteData = hash.getBytes();
-            String hash = HashUtils.getHexOfByteArray(byteData);
-            for (int i = 0; i < 2016; i++) {
-                md.update(hash.getBytes());
-                byteData = md.digest();
-                hash = HashUtils.getHexOfByteArray(byteData);
-            }
-
-            return byteData;
-
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Invalid algorithm specified.");
-        }
-        return null;
-    }
 }
