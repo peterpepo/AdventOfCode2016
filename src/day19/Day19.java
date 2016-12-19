7d9fc0a9b27438f433a5b3e2c8fade8e4d412e0e
Peter Pepo <peterpepo1@gmail.com>
2016-12-19 22:38:41 +0100

    Day-19

--- src/day19/Day19.java
+++ src/day19/Day19.java
@@ -1,5 +1,8 @@
 package day19;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day19 {
 
     public static void solve() {
@@ -63,70 +66,103 @@ public class Day19 {
 
     public static void solve2() {
 
-        // Number of elves, total
-        // Test input
-        int[] elves = new int[3017957];
-        // Puzzle01 input
-//        int[] elves = new int[3017957];
+        boolean[] elves = new boolean[3017957];
+
 
         // Give each elf one present
         for (int i = 0; i < elves.length; i++) {
-            elves[i] = 1;
+            elves[i] = true;
         }
 
         int currentElf = 0;
         boolean endOfTheGame = false;
         int elvesRemaining = elves.length;
-        
+
         while (!endOfTheGame) {
             currentElf = (currentElf % elves.length);
 
             // If the elf still has something, he's part of the game and can steal
-            if (elves[currentElf] != 0) {
-
-                int stolenPresentsCount = 0;
+            if (elves[currentElf]) {
 
                 // Find elve across the circle (if there are two, steal from closer)
                 int skipElves = Math.floorDiv(elvesRemaining, 2);
 //                System.out.println("Skip elves: "+skipElves);
-                int adjustedPosition=-1;
-                for(int i=0; i<elves.length-1; i++) {
+                int adjustedPosition = -1;
+                for (int i = 0; i < elves.length - 1; i++) {
                     adjustedPosition = (currentElf + i + 1) % elves.length;
-                    
-                    if(elves[adjustedPosition]!=0) {
+
+                    if (elves[adjustedPosition]) {
                         skipElves--;
                     }
-                    
+
                     // We found the right one
-                    if(skipElves==0) {
+                    if (skipElves == 0) {
                         break;
                     }
                 }
-                
-                stolenPresentsCount = elves[adjustedPosition];
-                elves[adjustedPosition]=0;
+
+                elves[adjustedPosition] = false;
                 elvesRemaining--;
 
-                
-                
-                
                 // If we didn't find anyone, the game is over
-                if (stolenPresentsCount == 0) {
+                if (elvesRemaining == 1) {
                     endOfTheGame = true;
                 }
 
                 // Increment elve's present count by amount, he just stole
-                elves[currentElf] += stolenPresentsCount;
             }
             currentElf++;
         }
 
         // Find elf, who has all the presents
         for (int i = 0; i < elves.length; i++) {
-            if (elves[i] != 0) {
-                System.out.println(i + 1 + ". elf has all " + elves[i] + " presents");
+            if (elves[i]) {
+                System.out.println(i + 1 + ". elf has all ");
             }
         }
 
+    }
+
+    public static void solve3() {
+        final Integer ELVES_COUNT = 3017957;
+        List<Integer> elves = new ArrayList<>();
+
+        for (Integer i = 0; i < ELVES_COUNT; i++) {
+            elves.add(i);
+        }
+
+        Integer currentElf = 0;
+        while (elves.size() > 1) {
+            currentElf = currentElf % elves.size();
+
+            int diameter = Math.floorDiv(elves.size(), 2);
+            int otherElf = (currentElf + diameter) % elves.size();
+
+            elves.set(currentElf, elves.get(currentElf) + elves.remove(otherElf));
+        }
+
+    }
+
+    public static void solve4() {
+        final Integer ELVES_COUNT = 3017957;
+        List<Integer> elves = new ArrayList<>();
+
+        for (Integer i = 0; i < ELVES_COUNT; i++) {
+            elves.add(i + 1);
+        }
+
+        Integer currentElfOrder = 0;
+        while (elves.size() > 1) {
+            currentElfOrder = currentElfOrder % elves.size();
+
+            int otherElfOrder = (currentElfOrder + (Math.floorDiv(elves.size(), 2))) % elves.size();
+
+            elves.remove(otherElfOrder);
+
+            if (otherElfOrder > currentElfOrder) {
+                currentElfOrder++;
+            }
+        }
+        System.out.println("Second puzzle: Elf #" + elves.get(0) + " has all presents");
     }
 }
---
+++ src/day19/Elf.java
@@ -0,0 +1,26 @@
+package day19;
+
+public class Elf {
+    private String name;
+//    private Integer presentsCount;
+
+    public Elf(String name) {
+        this.name = name;
+//        presentsCount = 1;
+    }
+
+//    public Integer getPresentsCount() {
+//        return presentsCount;
+//    }
+
+//    public void setPresentsCount(Integer presentsCount) {
+//        this.presentsCount = presentsCount;
+//    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+    
+}
