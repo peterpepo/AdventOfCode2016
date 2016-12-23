4ccf1325cfd8141304094e491b45148648925f06
Peter Pepo <peterpepo1@gmail.com>
2016-12-23 21:26:48 +0100

    Day-21: Cleanup
    * Refactors day21 solution
    * Cleans up test inputs

--- src/day21/Day21.java
+++ src/day21/Day21.java
@@ -8,14 +8,7 @@ import java.util.regex.Pattern;
 
 public class Day21 {
 
-    public static void solve() {
-        final String PUZZLE1_INPUT = "abcdefgh";
-
-        /*
-        PUZZLE - 01
-         */
-        Scrambler puzzle = new Scrambler(PUZZLE1_INPUT);
-
+    private static String solvePuzzle(Scrambler scrambler, List<String> instructions) {
         /*
         REGEXP PATTERNS FOR EACH OF COMMANDS
          */
@@ -28,125 +21,76 @@ public class Day21 {
         Pattern movePositionToPosition = Pattern.compile("move position (\\d)+ to position (\\d)+");
 
         Matcher m;
+        for (String s : instructions) {
+            m = swapPositionWithPosition.matcher(s);
+            if (m.find()) {
+                scrambler.swapByIndex(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
+                continue;
+            }
 
-        PuzzleInputReader firstPuzzleInput = new PuzzleInputReader("src/day21/Day21-puzzleInput.txt");
-//        // Load all intervals from puzzle input
-//        for (String s : firstPuzzleInput.getListOfLines()) {
-//            System.out.println(puzzle);
-//            System.out.println("-~-~-~-~-~");
-//            System.out.println(s);
-//            m = swapPositionWithPosition.matcher(s);
-//            if(m.find()) {
-//                puzzle.swapByIndex(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
-//                continue;
-//            }
-//            
-//            m = swapLetterWithLetter.matcher(s);
-//            if(m.find()) {
-//                puzzle.swapByChar(m.group(1).charAt(0), m.group(2).charAt(0));
-//                continue;
-//            }
-//            
-//            m = rotateLeftSteps.matcher(s);
-//            if(m.find()) {
-//                puzzle.rotateLeft(Integer.parseInt(m.group(1)));
-//                continue;
-//            }
-//            
-//            m = rotateRightSteps.matcher(s);
-//            if(m.find()) {
-//                puzzle.rotateRight(Integer.parseInt(m.group(1)));
-//                continue;
-//            }
-//            
-//            m = rotateBasedOnCharacter.matcher(s);
-//            if(m.find()) {
-//                puzzle.rotateBasedOnCharPosition(m.group(1).charAt(0));
-//                continue;
-//            }
-//                    
-//            m = reversePositionsFromTo.matcher(s);
-//            if(m.find()) {
-//                puzzle.reversePart(Integer.parseInt(m.group(1)),Integer.parseInt(m.group(2)));
-//                continue;
-//            }
-//            
-//            m = movePositionToPosition.matcher(s);
-//            if(m.find()) {
-//                puzzle.movePosition(Integer.parseInt(m.group(1)),Integer.parseInt(m.group(2)));
-//                continue;
-//            }
-//        }
-//System.out.println(puzzle);
+            m = swapLetterWithLetter.matcher(s);
+            if (m.find()) {
+                scrambler.swapByChar(m.group(1).charAt(0), m.group(2).charAt(0));
+                continue;
+            }
+
+            m = rotateLeftSteps.matcher(s);
+            if (m.find()) {
+                scrambler.rotateLeft(Integer.parseInt(m.group(1)));
+                continue;
+            }
+
+            m = rotateRightSteps.matcher(s);
+            if (m.find()) {
+                scrambler.rotateRight(Integer.parseInt(m.group(1)));
+                continue;
+            }
+
+            m = rotateBasedOnCharacter.matcher(s);
+            if (m.find()) {
+                scrambler.rotateBasedOnCharPosition(m.group(1).charAt(0));
+                continue;
+            }
+
+            m = reversePositionsFromTo.matcher(s);
+            if (m.find()) {
+                scrambler.reversePart(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
+                continue;
+            }
+
+            m = movePositionToPosition.matcher(s);
+            if (m.find()) {
+                scrambler.movePosition(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
+                continue;
+            }
+        }
+
+        return scrambler.toString();
+    }
+
+    public static void solve() {
 
         /*
-        char[] testChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-
-        for (char c : testChars) {
-            UnScrambler unScrambler = new UnScrambler(PUZZLE_INPUT);
-            System.out.println("Before: " + unScrambler);
-            unScrambler.rotateBasedOnCharPosition(c);
-            System.out.println("Scrambled: " + unScrambler);
-            unScrambler.unRotateBasedOnCharPosition(c);
-            System.out.println("UnScrambled: " + unScrambler);
-            System.out.println("~~~");
-        }
+        Common
          */
-        
-        
-        final String PUZZLE2_INPUT = "fbgdceah";
-        UnScrambler puzzle2 = new UnScrambler(PUZZLE2_INPUT);
-        // Load all intervals from puzzle input
-        List<String> reversedInstructions = firstPuzzleInput.getListOfLines();
+        PuzzleInputReader puzzleInput = new PuzzleInputReader("src/day21/Day21-puzzleInput.txt");
+
+        /*
+        PUZZLE - 01
+        result: bgfacdeh
+         */
+        Scrambler puzzle1 = new Scrambler("abcdefgh");
+        System.out.println("First puzzle: " + solvePuzzle(puzzle1, puzzleInput.getListOfLines()));
+
+        /*
+        PUZZLE - 02
+        result: abfdgceh
+         */
+        UnScrambler puzzle2 = new UnScrambler("bdgheacf");
+        List<String> reversedInstructions = puzzleInput.getListOfLines();
         Collections.reverse(reversedInstructions);
-        
-        for (String s : reversedInstructions) {
-            System.out.println(puzzle2);
-            System.out.println("-~-~-~-~-~");
-            System.out.println(s);
-            m = swapPositionWithPosition.matcher(s);
-            if(m.find()) {
-                puzzle2.swapByIndex(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
-                continue;
-            }
-            
-            m = swapLetterWithLetter.matcher(s);
-            if(m.find()) {
-                puzzle2.swapByChar(m.group(1).charAt(0), m.group(2).charAt(0));
-                continue;
-            }
-            
-            m = rotateLeftSteps.matcher(s);
-            if(m.find()) {
-                puzzle2.rotateLeft(Integer.parseInt(m.group(1)));
-                continue;
-            }
-            
-            m = rotateRightSteps.matcher(s);
-            if(m.find()) {
-                puzzle2.rotateRight(Integer.parseInt(m.group(1)));
-                continue;
-            }
-            
-            m = rotateBasedOnCharacter.matcher(s);
-            if(m.find()) {
-                puzzle2.rotateBasedOnCharPosition(m.group(1).charAt(0));
-                continue;
-            }
-                    
-            m = reversePositionsFromTo.matcher(s);
-            if(m.find()) {
-                puzzle2.reversePart(Integer.parseInt(m.group(1)),Integer.parseInt(m.group(2)));
-                continue;
-            }
-            
-            m = movePositionToPosition.matcher(s);
-            if(m.find()) {
-                puzzle2.movePosition(Integer.parseInt(m.group(1)),Integer.parseInt(m.group(2)));
-                continue;
-            }
-        }
-System.out.println(puzzle2);
+        System.out.println("Second puzzle: " + solvePuzzle(puzzle2, puzzleInput.getListOfLines()));
+
     }
 
 }
