1db49046418461f342b4159411f4ef9a6fb95b33
Peter Pepo <peterpepo1@gmail.com>
2016-12-26 14:48:03 +0100

    Day 17
    * Complete solution for Day 17

--- src/day17/Day17.java
+++ src/day17/Day17.java
@@ -3,8 +3,13 @@ package day17;
 import java.util.PriorityQueue;
 
 public class Day17 {
-    
+
     public static void solve() {
+        solve1();
+        solve2();
+    }
+
+    public static void solve1() {
         MazePosition initialMaze = new MazePosition();
 
         PriorityQueue<QueueNode<MazePosition>> mazeQueue = new PriorityQueue<>(new QueueNodeComparator());
@@ -13,14 +18,13 @@ public class Day17 {
         mazeQueue.add(new QueueNode(initialMaze, 0, 0));
 
         while (!mazeQueue.isEmpty()) {
-            System.out.println("[INFO]\tQueue size: " + mazeQueue.size());
+
             QueueNode<MazePosition> currentNode = mazeQueue.remove();
 
             // If we find solution in this node, print it and end
             if (currentNode.getContent().isSolution()) {
                 System.out.println("First puzzle: " + currentNode.getDistance() + ": " + currentNode.getContent().getPreviousSteps());
                 break;
-//                return currentNode.getDistance();
             }
 
             MazePosition newUp = currentNode.getContent().moveUp();
@@ -44,6 +48,55 @@ public class Day17 {
             }
 
         }
+    }
+
+    /*
+    Still room for optimisation (e.g check already longer solutions first, then shorter)
+    */
+    public static void solve2() {
+        int maxLength = 0;
+
+        MazePosition initialMaze = new MazePosition();
+
+        PriorityQueue<QueueNode<MazePosition>> mazeQueue = new PriorityQueue<>(new QueueNodeComparator());
+
+        // Start Node - Point[0,0]
+        mazeQueue.add(new QueueNode(initialMaze, 0, 0));
+
+        while (!mazeQueue.isEmpty()) {
+
+            QueueNode<MazePosition> currentNode = mazeQueue.remove();
+
+            if (currentNode.getContent().isSolution()) {
+                if (currentNode.getDistance() > maxLength) {
+                    maxLength = currentNode.getDistance();
+
+                }
+                continue;
+            }
+
+            MazePosition newUp = currentNode.getContent().moveUp();
+            if (newUp != null) {
+                mazeQueue.add(new QueueNode(newUp, currentNode.getDistance() + 1, currentNode.getDistance() + 1));
+            }
+
+            MazePosition newDown = currentNode.getContent().moveDown();
+            if (newDown != null) {
+                mazeQueue.add(new QueueNode(newDown, currentNode.getDistance() + 1, currentNode.getDistance() + 1));
+            }
+
+            MazePosition newLeft = currentNode.getContent().moveLeft();
+            if (newLeft != null) {
+                mazeQueue.add(new QueueNode(newLeft, currentNode.getDistance() + 1, currentNode.getDistance() + 1));
+            }
+
+            MazePosition newRight = currentNode.getContent().moveRight();
+            if (newRight != null) {
+                mazeQueue.add(new QueueNode(newRight, currentNode.getDistance() + 1, currentNode.getDistance() + 1));
+            }
+
+        }
+        System.out.println("Second puzzle: " + maxLength);
     }
 
 }
